c0a2a137cbfe0c5e0be9bba4c92215b1627843dc
Jiraphat Kerdmolee <177822415+Jiraphat2401@users.noreply.github.com>
2025-08-12 20:23:07 +0700

    practice array day2 map filter reduce includes for...of

--- practice array.js
+++ practice array.js
@@ -31,3 +31,39 @@ for(let i=0; i < fruits.length; i++){
 }
 
 //คำตอบทำเองครับผม
+
+// โจทย์ Array ระดับกลาง (วันนี้) จาก chat-gpt
+// โจทย์ 1:
+// ให้ numbers = [3, 7, 2, 9, 5]
+// ใช้ .map() เพื่อสร้าง array ใหม่ที่มีค่าทุกตัวคูณ 2 แล้วแสดงผล
+
+let numbers = [3, 7, 2, 9, 5]
+let double = numbers.map(n => n *2);
+console.log(numbers)//[3,7,2,9,5]
+console.log(double) //[6,14,4,18,10]
+
+// โจทย์ 2:
+// ใช้ .filter() เพื่อหาตัวเลขที่มากกว่า 4 จาก numbers แล้วแสดงผล
+
+let filter = numbers.filter(n => n > 4);
+console.log(filter) // [7,9,5]
+
+// โจทย์ 3:
+// ใช้ .reduce() เพื่อหาผลรวมของตัวเลขทั้งหมดใน numbers
+
+let reduce = numbers.reduce((a,c) => a + c,0);
+console.log(reduce) // 26
+
+// โจทย์ 4:
+// ให้ words = ["apple", "banana", "mango", "orange"]
+// ใช้ .includes() เพื่อตรวจสอบว่า "banana" อยู่ใน array หรือไม่ และแสดงผลเป็น true หรือ false
+
+let words = ["apple", "banana", "mango", "orange"]
+console.log(words.includes("banana")) // true
+
+// โจทย์ 5:
+// ใช้ for...of วนลูป words แล้วพิมพ์ความยาวของแต่ละคำ
+
+for (const i of words) {
+    console.log(i.length)
+} //5, 6, 5, 6
